68d5b5ab53e7291e52c6d22063afa4b3f8346f9c
Stefan <stefan.mandl1@gmail.com>
2016-03-25 11:44:36 +0100

    Update readme Format source code

--- FirmwareUploader/readme.md
+++ FirmwareUploader/readme.md
@@ -1,29 +1,96 @@
 
-The Uploader is not ready yet....
+# The uploader is not ready yet....
 
 
-Some research:
+## Some research:
 
+The windows tools writes a log file. A good source of information
 
-Step
+Steps
 
-1. Connect to bootloader in the chip
-2. Disable Watchdog
-3. Doing some EMI Setup
-4. Calibrate the PSRAM
-5. Upload (0x70007000,'INT_SYSRAM')
-6. Upload (0x10020000,'EXT_RAM')
-7. Start DA - Loader (0x70007000)
-8. Connect to the DA-Loader
-9. .......
-
-
+1. Connect to the bootloader inside the chip.
+2. Disable the watchdog
+3. Calibrate the internal PSRAM
+4. Power up the baseband
+4. Doing EMI remap
+5. Upload part 1  (INT_SYSRAM) of the DA-Loader  to 0x70007000
+6. Upload part 2  (EXT_RAM) of the DA-Loader to 0x10020000
+7. Start DA-Loader (0x70007000)
+8. Connect to the DA-Loader. Now the DA-Loader takes over the communication.
+9. DA-Loader starts to detect NOR/SF flash. Using the file **NOR_FLASH_TABLE**
+12. Upload **SEEED02A_DEMO_BOOTLOADER_V005_MT2502_MAUI_11CW1418SP5_W15_19.bin**
+13. Upload **EXT_BOOTLOADER**
+15. Upload **ROM**
+16. Upload **VIVA**
+20. Create FAT Partition  Start address = 0x00500000 Length 0x00afe000
+21. Disconnect
 
 Useful links
 
-http://www.bunniestudios.com/blog/?p=4297
-https://github.com/xobs/fernly
+<http://www.bunniestudios.com/blog/?p=4297>
 
+<https://github.com/xobs/fernly>
+
+<https://github.com/mtek-hack-hack/docs/wiki/FirmwareImageFormats>
+
+## udev rule
+
+Since Rephone is based on a Mediatek chip, ModemManager will, by default,
+try to treat it as a modem and make it available for network connections.
+This is undesirable.
+
+To work around this problem, create a udev rule under /etc/udev/rules.d/
+called 71-rephone.rules with the following contents:
+
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="0e8d",\
+        ATTRS{idProduct}=="0003",\
+        MODE="0660", SYMLINK+="rephone"
+
+    ACTION=="add|change", SUBSYSTEM=="usb",\
+        ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="0e8d",\
+        ATTRS{idProduct}=="0003",\
+        ENV{ID_MM_DEVICE_IGNORE}="1"
+
+## Upload process
+
+1. Install the udev rule
+
+We need in the same folder
+
+1. **NOR_FLASH_TABLE**
+2. **EXT_RAM**
+3. **INT_SYSRAM**
+4. **EX_BOOTLOADER**
+5. **SEEED02A_DEMO_BOOTLOADER_V005_MT2502_MAUI_11CW1418SP5_W15_19.bin**
+6. **ROM**
+7. **VIVA**
+
+
+
+1. Start the *firmwareUploader.py* (not ready yet )
+2. Power off the device and connect it to the USB port.
+3. The upload is running. The red led goes on dimmed.
+4. Disconnect the device from USB.
+
+
+
+
+## Troubleshoot
+
+- Sometimes the first connect fails. Disconnect the device and reconnect it.
+- If something goes wrong during upload. 
+	Disconnect the device from USB port and disconnect also the battery !!. Try again
+- Flashed a wrong a incomplete firmware. Wait 15 seconds for watchdog reset.
+
+If nothing helps. Use the official windows version.
+
+## To Do
+
+A better error handling. 
+
+Retry during upload a block
+
+May be we have some timing issues
 
 If you have a datasheet or some useful information
 
--- uploader.py
+++ uploader.py
@@ -13,38 +13,37 @@
 # this program; if not, write to the Free Software Foundation, Inc., 51 Franklin
 # Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import serial
-import time
-import sys
-import re
 import argparse
-import traceback
 import os
-
+import re
+import serial
+import sys
+import time
+import traceback
 
 
 class MTKModem(object):
 
 	WRITE_SIZE = 400
 
-	def __init__(self,port):
+	def __init__(self, port):
 		self.open(port)
 
-	def open(self,port):
+	def open(self, port):
 		self.ser = serial.Serial(port, 115200, timeout=5)
 		self.ser.flushInput()
 		self.ser.flushOutput()
         
 
-	def SendCommand(self,command, getline=True,ignoreError=False):
+	def SendCommand(self, command, getline=True, ignoreError=False):
 		print 'send: %s' % (command)
 		self.ser.write(command + '\r')
 		data = ''
 		if getline:
-			data=self.ReadLine(ignoreError)
+			data = self.ReadLine(ignoreError)
 		return data 
 
-	def SendCommandResult(self,command):
+	def SendCommandResult(self, command):
 		out = []	
 		print 'send: %s' % (command)
 		self.ser.write(command + '\r')
@@ -55,13 +54,13 @@ class MTKModem(object):
 				break
 			if data == 'ERROR':
 				raise Exception('Comm Error')
-			if data !='':
+			if data != '':
 				out.append(data)	
 			
 
 
 
-	def ReadLine(self,ignoreError):
+	def ReadLine(self, ignoreError):
 		while 1:
 			data = self.ser.readline().rstrip("\n\r")
 			if data == 'OK':
@@ -71,19 +70,19 @@ class MTKModem(object):
 					break
 				raise Exception('Comm Error')
 				
-			if data !='':
+			if data != '':
 				print data
 
-	def ListFiles(self,path):
+	def ListFiles(self, path):
 		
-		fileList = self.SendCommandResult('AT+EFSL="'+path.encode("utf-16-be").encode("hex") +'"')
+		fileList = self.SendCommandResult('AT+EFSL="' + path.encode("utf-16-be").encode("hex") + '"')
 		print "List path: %s" % path
 		for item in fileList:
-			diritem  = item.split(",")
+			diritem = item.split(",")
 			# get file size
 			filesize = diritem[1]
 			# attribute of file
-			attrib   = diritem[2]
+			attrib = diritem[2]
 			quoted = re.compile('"[^"]*"')
 			for value in quoted.findall(item):
 				# find filename
@@ -93,15 +92,15 @@ class MTKModem(object):
 			
 
 
-	def DeleteFile(self,pathFilename):
+	def DeleteFile(self, pathFilename):
 		# Folder operation Back to root folder
 		print 'Delete File %s' % pathFilename
 		self.SendCommand('AT+EFSF=3')
 		# send command ignore error if file does not exits
-		self.SendCommand('AT+EFSD="'+pathFilename.encode("utf-16-be").encode("hex") +'"',True,True)
+		self.SendCommand('AT+EFSD="' + pathFilename.encode("utf-16-be").encode("hex") + '"', True, True)
 		
 
-	def sendFile(self,filename):
+	def sendFile(self, filename):
    		if not os.path.isfile(filename):
 			raise Exception('Can not open file: %s') % (filename)
         
@@ -120,7 +119,7 @@ class MTKModem(object):
 		print 'Filename Path %s' % filenamePath
 
 		# open file for write
-		self.SendCommand('AT+EFSW=0,"'+ filenamePath.encode("hex") +'"')
+		self.SendCommand('AT+EFSW=0,"' + filenamePath.encode("hex") + '"')
 		
 		
 		# send data to open file
@@ -129,10 +128,10 @@ class MTKModem(object):
 			data = var.encode("hex")
 			if size == 400:
 				# last paket send eof
- 				self.SendCommand('AT+EFSW=2,1,400,"' +data + '"')
+ 				self.SendCommand('AT+EFSW=2,1,400,"' + data + '"')
 			else:
 				# send paket
-				self.SendCommand('AT+EFSW=2,0,400,"' +data + '"')
+				self.SendCommand('AT+EFSW=2,0,400,"' + data + '"')
 			size = size - 400
 	
 		if size > 0:
@@ -140,10 +139,10 @@ class MTKModem(object):
 			var = f.read(size)
 			data = var.encode("hex")
 			# send lastpaket
-			self.SendCommand('AT+EFSW=2,1,'+str(size) +',"' +data + '"')
+			self.SendCommand('AT+EFSW=2,1,' + str(size) + ',"' + data + '"')
 	
 		# close file
-		self.SendCommand('AT+EFSW=1,"' +filenamePath.encode("hex") +'"') 
+		self.SendCommand('AT+EFSW=1,"' + filenamePath.encode("hex") + '"') 
 
 
 
@@ -155,10 +154,10 @@ class MTKModem(object):
 def main():
 
 
-	parser = argparse.ArgumentParser(description = 'Push Application Utility', prog = 'uploader')
+	parser = argparse.ArgumentParser(description='Push Application Utility', prog='uploader')
 
-	parser.add_argument('--port', '-p',help = 'Serial port device',default = '/dev/ttyACM0')
-	#parser.add_argument('uploadfile', type=argparse.FileType('rb'), help='File for uploading')
+	parser.add_argument('--port', '-p', help='Serial port device', default='/dev/ttyACM0')
+	# parser.add_argument('uploadfile', type=argparse.FileType('rb'), help='File for uploading')
 
 	args = parser.parse_args()
 	if os.path.isfile('main.vxp') == False:
@@ -172,7 +171,7 @@ def main():
    	h.SendCommand('AT')
 
 	# exit all running process
-	h.SendCommand('AT+[666]EXIT_ALL',False)
+	h.SendCommand('AT+[666]EXIT_ALL', False)
 	time.sleep(8)
 	h.flushCom()
  	
@@ -181,15 +180,15 @@ def main():
 	h.SendCommand('AT+ESUO=3')
 		
 	# AT+EFSR FileRead
-	#h.SendCommand('AT+EFSR\r')
+	# h.SendCommand('AT+EFSR\r')
 
 	# File System Size  67 = C:\
-	#h.SendCommand('AT+EFS=67\r)
+	# h.SendCommand('AT+EFS=67\r)
 
 	# Folder operation Back to root folder
 	h.SendCommand('AT+EFSF=3')
 	
-	#h.DeleteFile('D:\\autostart.txt')
+	# h.DeleteFile('D:\\autostart.txt')
 
 	h.DeleteFile('C:\MRE\main.vxp')
 	
@@ -206,7 +205,7 @@ def main():
 	# Change operation mode to compatible
 	h.SendCommand('AT+ESUO=4')
 
-	h.SendCommand('AT+[666]REBOOT',False) 
+	h.SendCommand('AT+[666]REBOOT', False) 
 	
  	
 	# AT+EFSD Delete File
