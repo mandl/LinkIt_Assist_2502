61b97644ac9fd503856c97c3a51e84adb4c1cb15
Stefan <stefan.mandl1@gmail.com>
2016-07-24 11:04:40 +0200

    move to python3

--- FirmwareUploader/readHeader.py
+++ FirmwareUploader/readHeader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Pares the MTK fileinfo header
 #
@@ -33,43 +33,43 @@ def parseHeader(f):
   
     if (magic_ver & 0x00FFFFFF) != 0x004D4D4D:
         raise Exception('Did not found magic 4D4D4D')
-    print 'magic_ver 0x%x' % magic_ver
+    print ('magic_ver 0x%x' % magic_ver)
     
     # Total header size, incl. struct gfh_header 
     size, = struct.unpack('<H', f.read(2))
-    print 'header_size 0x%x' % size
+    print ('header_size 0x%x' % size)
     header_type, = struct.unpack('<H', f.read(2))
-    print 'header_type 0x%x' % header_type
+    print ('header_type 0x%x' % header_type)
     
-    print 'FILE_INFO'
+    print ('FILE_INFO')
     
     # /* 0 - gfh_file_info */
      # "FILE_INFO", zero-padded 
     f.read(12)
     
     file_ver, = struct.unpack('<I', f.read(4)) 
-    print 'file_ver 0x%x' % file_ver
+    print ('file_ver 0x%x' % file_ver)
     file_type, = struct.unpack('<H', f.read(2))
-    print 'file_type 0x%x' % file_type
+    print ('file_type 0x%x' % file_type)
     flash_dev, = struct.unpack('B', f.read(1))
-    print 'flash_dev 0x%x' % flash_dev
+    print ('flash_dev 0x%x' % flash_dev)
     sig_type = struct.unpack('B', f.read(1))
-    print 'sig_type 0x%x' % sig_type 
+    print ('sig_type 0x%x' % sig_type )
     load_addr, = struct.unpack('<I', f.read(4)) 
-    print 'load_addr 0x%x' % load_addr
+    print ('load_addr 0x%x' % load_addr)
     file_len, = struct.unpack('<I', f.read(4))
-    print 'file_len 0x%x' % file_len
+    print ('file_len 0x%x' % file_len)
     max_size, = struct.unpack('<I', f.read(4)) 
-    print 'max_size 0x%x' % max_size
+    print ('max_size 0x%x' % max_size)
     content_offset = struct.unpack('<I', f.read(4))
-    print 'content_offset 0x%x' % content_offset
+    print ('content_offset 0x%x' % content_offset)
     sig_len, = struct.unpack('<I', f.read(4)) 
-    print 'sig_len 0x%x' % sig_len
+    print ('sig_len 0x%x' % sig_len)
     sig_type, = struct.unpack('<I', f.read(4)) 
-    print 'sig_type 0x%x' % sig_type
+    print ('sig_type 0x%x' % sig_type)
     attr, = struct.unpack('<I', f.read(4))
-    print 'attr 0x%x' % attr 
-    print ''
+    print ('attr 0x%x' % attr) 
+    print ('')
     
     
 
@@ -87,23 +87,23 @@ def main():
     FilenameROM2 = Folder + "VIVA"
     
    
-    f = file(FilenameBootloader, 'rb')
+    f = open(FilenameBootloader, 'rb')
   
-    f2 = file(FilenameBootloaderExt, 'rb')
+    f2 = open(FilenameBootloaderExt, 'rb')
    
-    f3 = file(FilenameROM1, 'rb')
+    f3 = open(FilenameROM1, 'rb')
   
-    f4 = file(FilenameROM2, 'rb')
+    f4 = open(FilenameROM2, 'rb')
    
     
     
-    print  FilenameBootloader
+    print ( FilenameBootloader)
     parseHeader(f)
-    print FilenameBootloaderExt
+    print (FilenameBootloaderExt)
     parseHeader(f2)
-    print FilenameROM1
+    print (FilenameROM1)
     parseHeader(f3)
-    print FilenameROM2
+    print (FilenameROM2)
     parseHeader(f4)
     
   
@@ -112,6 +112,6 @@ if __name__ == '__main__':
     try:
         main()
        
-    except Exception, err:
-        sys.stderr.write('ERROR: %s\n' % str(err))
+    except Exception:
+        #sys.stderr.write('ERROR: %s\n' % str(err))
         traceback.print_exc()
